d4d5374234f70f7a8fc7de5fd0dbc7bcdc4e9e2d
Jpjaiprakash245@gmail.com <jpjaiprakash245@gmail.com>
2022-06-01 18:13:26 +0530

    handled conflict

--- src/test/java/Tests/Class1Test.java
+++ src/test/java/Tests/Class1Test.java
@@ -7,11 +7,20 @@ public class Class1Test {
 	public void employees() {
 		System.out.println("Yash");
 		System.out.println("Darshan");
+<<<<<<< HEAD
 		System.out.println("Sudeep");
 		//System.out.println("kishrkumar1");
 	
 		
 		//person bydhd
+=======
+		//System.out.println("Sudeep");
+		//System.out.println("Kishor");
+		
+	
+		//person 1
+		
+>>>>>>> d38b2568ef3c1998d12bbad512e2f2f3dd16f6fd
 	}
 
 }
